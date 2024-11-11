48e3864583828f257b00fc0a33ece6100c947789
IamSila <silamulingi@gmail.com>
2024-11-11 13:47:12 +0300

    pushed everything

--- Django/assignment/firstApp/views.py
+++ Django/assignment/firstApp/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    """defines the context and renders the request"""
     name = "sila Mulingi"
     context = {
         "name": name,
-        "phone number": 123456676,
+        "phone_number": 123456676,
         "age": 45
     }
     return render(request, "index.html", context)
